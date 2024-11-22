050fdaed2665c2b0ab5ebc053276fd1558b8f1b5
Simone Pelaia <simone.pelaia@casagit.eu>
2024-11-22 16:41:51 +0100

    u

---
+++ AppUtils.Test.Console/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AppUtils.Lib.Banca;
+
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("(1) - Test Iban");
+var iban = new Iban("IT001234567890000000000000");
+Console.WriteLine($"Esito iban: {iban.ValidateFormalNoException()}");
